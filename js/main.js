ffb2abfa30c4b8f5232c82e3511867758084e889
Ezequiel Criscueli <criscuelieduardo@gmail.com>
2023-06-04 22:32:03 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const categoria = document.querySelector("#categoria")
+const cantidad = document.querySelector("#cantidad")
+const totalAPagar = document.querySelector("#totalAPagar")
+
+function resumen(){
+    let resultado
+
+    switch (categoria.value) {
+        case "Estudiante":
+            resultado = (200 - (200 * 0.8)) * cantidad.value
+            break;
+        case "Trainee":
+            resultado = (200 - (200 * 0.5)) * cantidad.value
+            break;
+        case "Junior":
+            resultado = (200 - (200 * 0.15)) * cantidad.value
+            break
+        default:
+            break;
+    }
+    totalAPagar.value = `Total a pagar: $${resultado}`
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+Trabajo práctico integrador JS
+Codo a Codo Primer Cuatrimestre 2023 Comisión 23069
+
+Consignas:
+
+- Deberán replicar la siguiente sección (es una sección que se debe agregar al integrador front-End) al presionar en el enlace “tickets”.
+- Deberá contener la funcionalidad en Javascript, al momento de presionar el botón “Resumen”, deberá mostrar en la sección “Total a Pagar: $”, el monto correspondiente a la cantidad de tickets a comprar con el descuento correspondiente dependiendo la categoría seleccionada, existen 3 categorías, Estudiante, Trainee, Junior.
